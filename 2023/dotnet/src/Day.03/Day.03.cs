a431c53d0eea2b6fd9dfcbd4459fb0b76f90d56f
Liam Reimers <liam.reimers@gmail.com>
2024-05-21 19:07:45 -0700

    Read data from file.

--- 2023/dotnet/src/Day.03/Day.03.cs
+++ 2023/dotnet/src/Day.03/Day.03.cs
@@ -8,7 +8,6 @@ Console.WriteLine("Advent of Code 2023 - Day 3");
 List<string> inputData = new List<string>();
 using StreamReader reader = new("var/day_03/sample.txt");
 string? gameRecord;
-while ((gameRecord = reader.ReadLine()) != null)
-{
+while ((gameRecord = reader.ReadLine()) != null) {
     inputData.Add(gameRecord);
 }
