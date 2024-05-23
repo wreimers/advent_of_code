6c8d6f27e2f171cda65d376d2bf3a556a50fc68d
Liam Reimers <liam.reimers@gmail.com>
2024-05-23 13:28:58 -0700

    Add read input file boilerplate.

--- 2023/dotnet/src/Day.03/Day.03.cs
+++ 2023/dotnet/src/Day.03/Day.03.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.NetworkInformation;
+using System.Runtime.CompilerServices;
+using System.Collections.Generic;   
+
+Console.WriteLine("Advent of Code 2023 - Day 3");
+
+// Load data from file
+List<string> inputData = new List<string>();
+using StreamReader reader = new("var/day_03/sample.txt");
+string? gameRecord;
+while ((gameRecord = reader.ReadLine()) != null)
+{
+    inputData.Add(gameRecord);
+}
