37363f33aeb8278c7872cbe851b9f2ff5d96cb8f
Liam Reimers <liam.reimers@gmail.com>
2024-05-27 13:12:53 -0700

    Read sample data

--- 2023/dotnet/src/Day.04/Day.04.cs
+++ 2023/dotnet/src/Day.04/Day.04.cs
@@ -14,10 +14,23 @@ namespace Day04
         static void Main_Day4_Part2(string[] args) 
         {
             Console.WriteLine("Advent of Code 2023 Day 4 Part 2");
-            using StreamReader reader = new("var/day_04/input.txt");
+            using StreamReader reader = new("var/day_04/sample.txt");
+            int row = 1;
+            string? rawLine;
             while ((rawLine = reader.ReadLine()) != null)
             {
-                
+                Console.WriteLine($"({row}) {rawLine}");
+                char[] splitters = [':', '|', ];
+                string[] tokens = rawLine.Split(splitters, StringSplitOptions.RemoveEmptyEntries);
+                string[] winningNumbers = tokens[1].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string[] haveNumbers = tokens[2].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                int matches = 0;
+                foreach (string number in haveNumbers ) {
+                    if (winningNumbers.Contains(number)) {
+                        matches += 1;
+                    }
+                }
+                row += 1;
             }
         }
 
