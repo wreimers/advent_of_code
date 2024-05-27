2d2708053cabbc2c1b591381fdc3a07f2b6f8fd3
Liam Reimers <liam.reimers@gmail.com>
2024-05-27 14:02:56 -0700

    Day 4 part 2 working for sample data

--- 2023/dotnet/src/Day.04/Day.04.cs
+++ 2023/dotnet/src/Day.04/Day.04.cs
@@ -17,21 +17,38 @@ namespace Day04
             using StreamReader reader = new("var/day_04/sample.txt");
             int row = 1;
             string? rawLine;
+            var gameCards = new List<GameCard>();
             while ((rawLine = reader.ReadLine()) != null)
             {
                 Console.WriteLine($"({row}) {rawLine}");
+                var card = new GameCard { row=row, text=rawLine, };
+                gameCards.Add(card);
                 char[] splitters = [':', '|', ];
                 string[] tokens = rawLine.Split(splitters, StringSplitOptions.RemoveEmptyEntries);
-                string[] winningNumbers = tokens[1].Split(' ', StringSplitOptions.RemoveEmptyEntries);
-                string[] haveNumbers = tokens[2].Split(' ', StringSplitOptions.RemoveEmptyEntries);
-                int matches = 0;
-                foreach (string number in haveNumbers ) {
-                    if (winningNumbers.Contains(number)) {
-                        matches += 1;
+                card.winningNumbers = tokens[1].Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+                card.haveNumbers = tokens[2].Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+                foreach (string number in card.haveNumbers ) 
+                {
+                    if (card.winningNumbers.Contains(number)) 
+                    {
+                        card.matches += 1;
                     }
                 }
+                Console.WriteLine($"({row}) matches {card.matches}");
                 row += 1;
             }
+            row = 1;
+            int totalMagnitude = 0;
+            foreach (GameCard card in gameCards) 
+            {
+                Console.WriteLine($"({row}) magnitude {card.magnitude}");
+                for (int tmpMatches = card.matches; tmpMatches > 0; tmpMatches -= 1) {
+                    gameCards[row-1+tmpMatches].magnitude += (1 * card.magnitude);
+                }
+                totalMagnitude += card.magnitude;
+                row += 1;
+            }
+            Console.WriteLine($"Total magnitude {totalMagnitude}");
         }
 
         static void Main_Day4_Part1(string[] args)
@@ -65,4 +82,15 @@ namespace Day04
             Console.WriteLine($"Sum: {sum}");
         }
     }
+
+    public class GameCard {
+        public required int row {get; set;}
+        public required string text {get; set;}
+        public List<string> winningNumbers = new List<string>();
+        public List<string> haveNumbers = new List<string>();
+        public int matches = 0;
+        public int magnitude = 1;
+
+    }
+
 }
