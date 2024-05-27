95148cdc3e3265f88e5eabeaf17f4e870ddb7347
Liam Reimers <liam.reimers@gmail.com>
2024-05-26 18:12:16 -0700

    Scaffold part 2

--- 2023/dotnet/src/Day.03/Day.03.cs
+++ 2023/dotnet/src/Day.03/Day.03.cs
@@ -9,7 +9,28 @@ namespace Day03
 {
     internal class Program
     {
+
         static void Main(string[] args)
+        {
+            Main_Day3_Part2(args);
+        }
+
+        static void Main_Day3_Part2(string[] args) {
+            Console.WriteLine("Advent of Code 2023 - Day 3 Part 2");
+            // Load data from file
+            using StreamReader reader = new("var/day_03/input.txt");
+            int row = 0;
+            var partNumbers = new List<PartNumber>();
+            SchematicLine? previousLine = null;
+            string? rawLine;
+            while ((rawLine = reader.ReadLine()) != null)
+            {
+                Console.WriteLine($"({row}) {rawLine}");
+                row += 1;
+            }
+        }
+
+        static void Main_Day3_Part1(string[] args)
         {
 
             Console.WriteLine("Advent of Code 2023 - Day 3 Part 1");
