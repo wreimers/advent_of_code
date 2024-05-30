ad064460f52888ffdc3d369ccc1516d3f4133a5a
Liam Reimers <liam.reimers@gmail.com>
2024-05-30 14:06:20 -0700

    Stub in main() source

--- 2023/dotnet/src/Day.06/Day.06.cs
+++ 2023/dotnet/src/Day.06/Day.06.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Reflection.PortableExecutable;
+using System.Runtime.CompilerServices;
+
+namespace Day06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Main_Day6_Part1(args);
+        }
+
+        static void Main_Day6_Part1(string[] args) 
+        {
+            Console.WriteLine("Advent of Code 2023 Day 6 Part 1");
+        }
+    }
+}
