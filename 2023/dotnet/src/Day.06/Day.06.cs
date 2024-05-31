3dc454ac86e0b7b8b1de8f4419c2d738878bc8af
Liam Reimers <liam.reimers@gmail.com>
2024-05-30 18:15:50 -0700

    Working for sample data

--- 2023/dotnet/src/Day.06/Day.06.cs
+++ 2023/dotnet/src/Day.06/Day.06.cs
@@ -14,6 +14,30 @@ namespace Day06
         static void Main_Day6_Part1(string[] args) 
         {
             Console.WriteLine("Advent of Code 2023 Day 6 Part 1");
+
+            // Time:      7  15   30
+            // Distance:  9  40  200
+            var races = new List<Race>();
+            races.Add(new Race {time=7, distance=9});
+            races.Add(new Race {time=15, distance=40});
+            races.Add(new Race {time=30, distance=200});
+
+            double product = 1;
+            foreach(Race race in races) {
+                var winningTimes = new List<double>();
+                for (double time=0; time<race.time; time += 1) {
+                    double travelTime = race.time - time;
+                    double velocity = time;
+                    double travelDistance = travelTime * velocity;
+                    if (travelDistance > race.distance) {
+                        winningTimes.Add(time);
+                    }
+                }
+                product *= winningTimes.Count;
+                Console.WriteLine($"winningTimes {String.Join(", ", winningTimes)}");
+                Console.WriteLine($"product      {product}");
+            }
         }
     }
+
 }
