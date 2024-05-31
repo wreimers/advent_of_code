fc7b0cbc9198b0101715e6bb8b48d54c8387628f
Liam Reimers <liam.reimers@gmail.com>
2024-05-30 18:33:38 -0700

    Stub in program files

---
+++ 2023/dotnet/src/Day.07/Day.07.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Reflection.PortableExecutable;
+using System.Runtime.CompilerServices;
+
+namespace Day07
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Main_Day7_Part1(args);
+        }
+
+        static void Main_Day7_Part1(string[] args) 
+        {
+            Console.WriteLine("Advent of Code 2023 Day 7 Part 1");
+        }
+    }
+}
