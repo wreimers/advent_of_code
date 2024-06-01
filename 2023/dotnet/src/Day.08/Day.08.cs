436225b74ca6759cb3866812d64b4376592caaa2
Liam Reimers <liam.reimers@gmail.com>
2024-06-01 07:12:47 -0700

    Stub out day 8 part 2

--- 2023/dotnet/src/Day.08/Day.08.cs
+++ 2023/dotnet/src/Day.08/Day.08.cs
@@ -8,10 +8,63 @@ namespace Day08
     {
         static void Main(string[] args)
         {
-            Main_Day8_Part1(args);
+            Main_Day8_Part2(args);
         }
 
-        static void Main_Day8_Part1(string[] args) 
+        static void Main_Day8_Part2(string[] args) 
+        {
+            Console.WriteLine("Advent of Code 2023 Day 8 Part 2");
+            var networkNodes = new Dictionary<string, NetworkNode>();
+            
+            string? rawLine;
+            using StreamReader reader = new("var/day_08/input.txt");
+            string? rawInstructions = reader.ReadLine();
+            var instructions = rawInstructions.ToCharArray().ToList();
+            while ((rawLine = reader.ReadLine()) != null)
+            {
+                Console.WriteLine($"{rawLine}");
+                if (rawLine == "")
+                {
+                    continue;
+                }
+                char[] splitters = [' ', '=', '(', ',', ')', ];
+                string[] tokens = rawLine.Split(splitters, StringSplitOptions.RemoveEmptyEntries);
+                string name = tokens[0];
+                string left = tokens[1];
+                string right = tokens[2];
+                NetworkNode node = new NetworkNode {name=name, left=left, right=right};
+                networkNodes[name] = node;
+            }
+
+            var currentNode = "AAA";
+            var targetNode = "ZZZ";
+            int steps = 0;
+            int instructionIndex = 0;
+            while (currentNode != targetNode) {
+                Console.WriteLine($"currentNode {currentNode}");
+                steps += 1;
+                var node = networkNodes[currentNode];
+                if (instructionIndex == instructions.Count)
+                {
+                    instructionIndex = 0;
+                }
+                var instruction = instructions[instructionIndex];
+                instructionIndex += 1;
+                if (instruction == 'L')
+                {
+                    currentNode = node.left;
+                    Console.WriteLine($"going left to {node.left}");
+                }
+                else if (instruction == 'R')
+                {
+                    currentNode = node.right;
+                    Console.WriteLine($"going right to {node.right}");
+                }
+            }
+            Console.WriteLine($"steps {steps}");
+        }
+
+static void Main_Day8_Part1(string[] args) 
         {
             Console.WriteLine("Advent of Code 2023 Day 8 Part 1");
             var networkNodes = new Dictionary<string, NetworkNode>();
@@ -63,6 +116,7 @@ namespace Day08
             }
             Console.WriteLine($"steps {steps}");
         }
+
     }
 
     public class NetworkNode {
