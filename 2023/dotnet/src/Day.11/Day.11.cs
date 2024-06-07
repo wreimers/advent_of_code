9d44cfdf01af5017ac3d516cdf8d2c0a06a0f4d2
Liam Reimers <liam.reimers@gmail.com>
2024-06-07 13:55:08 -0700

    day_11 wip

--- 2023/dotnet/src/Day.11/Day.11.cs
+++ 2023/dotnet/src/Day.11/Day.11.cs
@@ -14,14 +14,36 @@ namespace Day11
         static void Main_Day11(string[] args)
         {
             Console.WriteLine("Advent of Code 2023 Day 11");
-            string? rawLine;
             using StreamReader reader = new("var/day_11/sample.txt");
-            int row = 0;
+            string? rawLine = reader.ReadLine();
+            if (rawLine is null) { throw new Exception("WHY IS THE FILE EMPTY"); }
+            char[][] grid = new char[rawLine.Length][];
+            // row 0
+            grid[0] = new char[rawLine.Length];
+            transcribe(rawLine, grid[0]);
+            int row = 1;
             while ((rawLine = reader.ReadLine()) != null)
             {
+                grid[row] = new char[rawLine.Length];
+                transcribe(rawLine, grid[row]);
                 row += 1;
-                Console.WriteLine($"{rawLine}");
+                // Console.WriteLine($"{rawLine}");
+                Console.WriteLine($"{String.Join("", grid[row-1].ToList())}");
+            }
+            // expand rows
+            for (int row=0; row<grid.Rank; row+=1) {
+                
             }
         }
-    }
-}
+
+        static public void transcribe(string? line, char[] gridRow) {
+            if (line is null) { throw new Exception("WHY IS THE STRING EMPTY"); }
+            for (int i=0; i<line.Length; i+=1)
+            {
+                gridRow[i] = line.ToCharArray()[i];
+            }
+        }
+
+    } // program
+} // namespace
+
