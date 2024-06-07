90030d1c7e1f87217d74a48ce64fa77e09657829
Liam Reimers <liam.reimers@gmail.com>
2024-06-07 13:13:40 -0700

    day_11 add main function that reads sample data

--- 2023/dotnet/src/Day.11/Day.11.cs
+++ 2023/dotnet/src/Day.11/Day.11.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Reflection.PortableExecutable;
+using System.Runtime.CompilerServices;
+
+namespace Day11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Main_Day11(args);
+        }
+
+        static void Main_Day11(string[] args)
+        {
+            Console.WriteLine("Advent of Code 2023 Day 11");
+            string? rawLine;
+            using StreamReader reader = new("var/day_11/sample.txt");
+            int row = 0;
+            while ((rawLine = reader.ReadLine()) != null)
+            {
+                row += 1;
+                Console.WriteLine($"{rawLine}");
+            }
+        }
+    }
+}
