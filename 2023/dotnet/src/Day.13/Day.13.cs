2d5fe1c0c993d0b979d89ef6f0c00734df32a0d3
Liam Reimers <liam.reimers@gmail.com>
2024-06-25 19:47:09 -0700

    day_13 plumb in the files

---
+++ 2023/dotnet/src/Day.13/Day.13.cs
@@ -0,0 +1,17 @@
+﻿namespace Day13
+{
+    internal class Program
+    {
+        private static string DATA_FILE = "var/day_13/sample.txt";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Advent of Code 2023 Day 13");
+            Main_Day13(args);
+        }
+
+        static void Main_Day13(string[] args)
+        {
+        }
+    }
+}
