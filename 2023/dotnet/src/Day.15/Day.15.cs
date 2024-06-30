b8b4f988e0007bbb5af1c88c3c5ed2a76919d3af
Liam Reimers <liam.reimers@gmail.com>
2024-06-30 16:59:23 -0700

    day_15 wip - part 2

--- 2023/dotnet/src/Day.15/Day.15.cs
+++ 2023/dotnet/src/Day.15/Day.15.cs
@@ -1,9 +1,46 @@
-﻿namespace Day15
+﻿using System.Text.RegularExpressions;
+
+namespace Day15
 {
     internal class Program
     {
         private static string DATA_FILE = "var/day_15/sample.txt";
 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Advent of Code 2023 Day 15");
+            using StreamReader reader = new(DATA_FILE);
+            string? rawLine = reader.ReadLine();
+            if (rawLine is not null)
+            {
+                Console.WriteLine($"rawLine:{rawLine}");
+                char[] splitters = [',',];
+                string[] tokens = rawLine.Split(splitters, StringSplitOptions.RemoveEmptyEntries);
+                // int grandTotal = 0;
+                string pattern = @"^([a-z]+)([\=\-]+)(\d*)$";
+                Regex rg = new Regex(pattern, RegexOptions.IgnoreCase);
+                foreach (string t in tokens)
+                {
+                    Match m = rg.Match(t);
+                    if (!m.Success)
+                    {
+                        throw new Exception("REGEX DIDN'T MATCH");
+                    }
+                    var label = m.Groups[1].ToString();
+                    var operation = m.Groups[2].ToString();
+                    var focalLength = m.Groups[3].ToString();
+                    Console.WriteLine($"label:{label} operation:{operation} focalLength:{focalLength}");
+                    // int value = calculateHASH(t);
+                    // grandTotal += value;
+
+
+                }
+                // Console.WriteLine($"grandTotal:{grandTotal}");
+            }
+
+        }
+
+        /* part 1
         static void Main(string[] args)
         {
             Console.WriteLine("Advent of Code 2023 Day 15");
@@ -24,6 +61,7 @@
             }
 
         }
+        */
 
         public static int calculateHASH(string word)
         {
@@ -42,3 +80,7 @@
     }
 }
 
+public class LensBox
+{
+
+}
