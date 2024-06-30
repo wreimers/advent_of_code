03adf2445bc98531552aad30e5ca85f7cba99ecf
Liam Reimers <liam.reimers@gmail.com>
2024-06-29 22:12:50 -0700

    day_14 wip - works for sample data. 1_000_000_000 cycles is going to take forever.

--- 2023/dotnet/src/Day.14/Day.14.cs
+++ 2023/dotnet/src/Day.14/Day.14.cs
@@ -2,7 +2,7 @@
 {
     internal class Program
     {
-        private static string DATA_FILE = "var/day_14/input.txt";
+        private static string DATA_FILE = "var/day_14/sample.txt";
 
         static void Main(string[] args)
         {
@@ -12,7 +12,7 @@
 
         static void Main_Day14(string[] args)
         {
-            var p = new Puzzle();
+            var p = new MirrorPuzzle();
             string? rawLine;
             using StreamReader reader = new(DATA_FILE);
             while ((rawLine = reader.ReadLine()) != null)
@@ -21,25 +21,53 @@
             }
             Console.WriteLine();
             p.display();
-            var mirrors = new List<Mirror>();
-            for (int row = 0; row < p.rowCount; row += 1)
+
+            // long cycles = 1_000_000_000;
+            // long cycles = 10_000_000;
+            long cycles = 3;
+            for (long k = 0; k < cycles; k += 1)
             {
-                for (int col = 0; col < p.colCount; col += 1)
+                foreach (Mirror m in p.mirrorsNorthToSouth)
                 {
-                    if (p.grid[row, col] == 'O')
+                    while (m.north is not null && m.north == '.')
                     {
-                        // Console.WriteLine($"mirror row:{row} col:{col}");
-                        mirrors.Add(new Mirror { row = row, col = col, grid = p.grid });
+                        m.goNorth();
                     }
                 }
-            }
-            int score = 0;
-            foreach (Mirror m in mirrors)
-            {
-                while (m.north is not null && m.north == '.')
+                // Console.WriteLine();
+                // p.display();
+                foreach (Mirror m in p.mirrorsWestToEast)
                 {
-                    m.goNorth();
+                    while (m.west is not null && m.west == '.')
+                    {
+                        m.goWest();
+                    }
                 }
+                // Console.WriteLine();
+                // p.display();
+                foreach (Mirror m in p.mirrorsSouthToNorth)
+                {
+                    while (m.south is not null && m.south == '.')
+                    {
+                        m.goSouth();
+                    }
+                }
+                // Console.WriteLine();
+                // p.display();
+                foreach (Mirror m in p.mirrorsEastToWest)
+                {
+                    while (m.east is not null && m.east == '.')
+                    {
+                        m.goEast();
+                    }
+                }
+                // Console.WriteLine();
+                // p.display();
+            }
+
+            int score = 0;
+            foreach (Mirror m in p.mirrorsNorthToSouth)
+            {
                 score += p.rowCount - m.row;
             }
             Console.WriteLine();
@@ -51,24 +79,150 @@
     }
 }
 
+public enum Direction
+{
+    North,
+    East,
+    South,
+    West,
+}
+
 public class Mirror
 {
     required public int row { get; set; }
     required public int col { get; set; }
-    required public char[,] grid { get; set; }
+    required public MirrorPuzzle puzzle { get; set; }
     public char? north
     {
         get
         {
             if (row == 0) { return null; }
-            return grid[row - 1, col];
+            return puzzle.grid[row - 1, col];
+        }
+    }
+    public char? south
+    {
+        get
+        {
+            if (row == puzzle.rowCount - 1) { return null; }
+            return puzzle.grid[row + 1, col];
+        }
+    }
+    public char? west
+    {
+        get
+        {
+            if (col == 0) { return null; }
+            return puzzle.grid[row, col - 1];
+        }
+    }
+    public char? east
+    {
+        get
+        {
+            if (col == puzzle.colCount - 1) { return null; }
+            return puzzle.grid[row, col + 1];
         }
     }
     public void goNorth()
     {
-        grid[row, col] = '.';
+        puzzle.grid[row, col] = '.';
         row = row - 1;
-        grid[row, col] = 'O';
+        puzzle.grid[row, col] = 'O';
+    }
+    public void goSouth()
+    {
+        puzzle.grid[row, col] = '.';
+        row = row + 1;
+        puzzle.grid[row, col] = 'O';
+    }
+    public void goWest()
+    {
+        puzzle.grid[row, col] = '.';
+        col = col - 1;
+        puzzle.grid[row, col] = 'O';
+    }
+    public void goEast()
+    {
+        puzzle.grid[row, col] = '.';
+        col = col + 1;
+        puzzle.grid[row, col] = 'O';
+    }
+}
+
+public class MirrorPuzzle : Puzzle
+{
+    public List<Mirror> mirrorsNorthToSouth
+    {
+        get
+        {
+            var mirrors = new List<Mirror>();
+            for (int row = 0; row < rowCount; row += 1)
+            {
+                for (int col = 0; col < colCount; col += 1)
+                {
+                    if (grid[row, col] == 'O')
+                    {
+                        mirrors.Add(new Mirror { row = row, col = col, puzzle = this });
+                    }
+                }
+            }
+            return mirrors;
+        }
+    }
+    public List<Mirror> mirrorsSouthToNorth
+    {
+        get
+        {
+            var mirrors = new List<Mirror>();
+            for (int row = rowCount - 1; row >= 0; row -= 1)
+            {
+                for (int col = 0; col < colCount; col += 1)
+                {
+                    if (grid[row, col] == 'O')
+                    {
+                        mirrors.Add(new Mirror { row = row, col = col, puzzle = this });
+                    }
+                }
+            }
+            return mirrors;
+        }
+    }
+    public List<Mirror> mirrorsWestToEast
+    {
+        get
+        {
+            var mirrors = new List<Mirror>();
+            for (int col = 0; col < colCount; col += 1)
+            {
+                for (int row = 0; row < rowCount; row += 1)
+                {
+                    if (grid[row, col] == 'O')
+                    {
+                        mirrors.Add(new Mirror { row = row, col = col, puzzle = this });
+                    }
+                }
+            }
+            return mirrors;
+        }
+    }
+    public List<Mirror> mirrorsEastToWest
+    {
+        get
+        {
+            var mirrors = new List<Mirror>();
+            for (int col = colCount - 1; col >= 0; col -= 1)
+            {
+                for (int row = 0; row < rowCount; row += 1)
+                {
+                    if (grid[row, col] == 'O')
+                    {
+                        mirrors.Add(new Mirror { row = row, col = col, puzzle = this });
+                    }
+                }
+            }
+            return mirrors;
+        }
     }
 }
 
