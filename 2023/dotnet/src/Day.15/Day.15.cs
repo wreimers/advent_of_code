fe67228dc3d5207a25f86b3ddbd57c844a71ce36
Liam Reimers <liam.reimers@gmail.com>
2024-06-30 19:45:14 -0700

    day_15 wip - part 2 works with input data - answer is 288521

--- 2023/dotnet/src/Day.15/Day.15.cs
+++ 2023/dotnet/src/Day.15/Day.15.cs
@@ -5,7 +5,7 @@ namespace Day15
 {
     internal class Program
     {
-        private static string DATA_FILE = "var/day_15/sample.txt";
+        private static string DATA_FILE = "var/day_15/input.txt";
 
         static void Main(string[] args)
         {
