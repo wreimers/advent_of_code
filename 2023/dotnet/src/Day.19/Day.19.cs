230e135d5e70869f1e1aa4157e2247f6514701b2
Liam Reimers <liam.reimers@gmail.com>
2024-07-03 20:37:08 -0700

    day_19 wip

---
+++ 2023/dotnet/src/Day.19/Day.19.cs
@@ -0,0 +1,12 @@
+﻿namespace Day19
+{
+    internal class Program
+    {
+        private static string DATA_FILE = "var/day_19/sample.txt";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Advent of Code 2023 Day 19");
+        }
+    }
+}
