c2067d742a4f0f9aea77a6a01448cc80b51216c1
Liam Reimers <liam.reimers@gmail.com>
2024-12-10 16:39:32 -0800

    Play with associated functions

--- 2024/rust/src/main.rs
+++ 2024/rust/src/main.rs
@@ -6,18 +6,14 @@ mod prelude {
 
 use crate::prelude::*;
 
-// struct SumArgs {
-//     n1: i32,
-//     n2: i32,
-// }
+struct SumArgs {
+    n1: i32,
+    n2: i32,
+}
 
-struct SumArgs(i32, i32);
-
-fn add_numbers(args: &SumArgs) -> i32 {
-    // args.n1 + args.n2
-    // args.0 + args.1
-    match args {
-        SumArgs(n1, n2) => n1 + n2
+impl SumArgs {
+    fn add_numbers(args: &SumArgs) -> i32 {
+        args.n1 + args.n2
     }
 }
 
@@ -25,10 +21,8 @@ fn main() {
     let _ms = MyStruct {};
     let _as = Another {};
 
-    // let args = SumArgs {n1: 10, n2: 20};
-    let args = SumArgs(10, 20);
-    let sum = add_numbers(&args);
+    let args = SumArgs { n1: 10, n2: 20 };
+    let sum = SumArgs::add_numbers(&args);
 
-    // println!("{} + {} = {}", args.n1, args.n2, sum);
-    println!("{} + {} = {}", args.0, args.1, sum);
+    println!("{} + {} = {}", args.n1, args.n2, sum);
 }
