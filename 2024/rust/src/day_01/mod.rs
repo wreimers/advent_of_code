586ad63a394183e08e4f5ef7e3cf519a6aa7c255
Liam Reimers <liam.reimers@gmail.com>
2024-12-10 19:20:29 -0800

    Get set up for day 1 part 1

--- 2024/rust/src/day_01/mod.rs
+++
@@ -1,6 +0,0 @@
-mod another;
-pub use another::Another;
-
-// use crate::prelude::*;
-
-pub struct MyStruct {}
--- 2024/rust/src/main.rs
+++ 2024/rust/src/main.rs
@@ -1,32 +1,6 @@
-mod day_01;
-
-mod prelude {
-    pub use crate::day_01::{MyStruct,Another};
-}
-
-use crate::prelude::*;
-
-struct SumArgs {
-    n1: i32,
-    n2: i32,
-}
-
-impl SumArgs {
-    fn new(n1: i32, n2: i32) -> Self {
-        Self { n1, n2 }
-    }
-
-    fn add_numbers(&self) -> i32 {
-        self.n1 + self.n2
-    }
-}
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
 fn main() {
-    let _ms = MyStruct {};
-    let _as = Another {};
-
-    let args = SumArgs::new(10, 20);
-    let sum = args.add_numbers();
-
-    println!("{} + {} = {}", args.n1, args.n2, sum);
+    let f = File::open("var/day_01_part_01_input.txt");
 }
