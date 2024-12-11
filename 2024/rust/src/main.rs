a44bfcc4ea58e6aa0fb7fcc84105983a338686e6
Liam Reimers <liam.reimers@gmail.com>
2024-12-10 16:35:26 -0800

    Play with tuple structs

--- 2024/rust/src/main.rs
+++ 2024/rust/src/main.rs
@@ -6,21 +6,26 @@ mod prelude {
 
 use crate::prelude::*;
 
-struct SumArgs {
-    n1: i32,
-    n2: i32,
-}
+// struct SumArgs {
+//     n1: i32,
+//     n2: i32,
+// }
+
+struct SumArgs(i32, i32);
 
 fn add_numbers(args: &SumArgs) -> i32 {
-    args.n1 + args.n2
+    // args.n1 + args.n2
+    args.0 + args.1
 }
 
 fn main() {
     let _ms = MyStruct {};
     let _as = Another {};
 
-    let args = SumArgs {n1: 10, n2: 20};
+    // let args = SumArgs {n1: 10, n2: 20};
+    let args = SumArgs(10, 20);
     let sum = add_numbers(&args);
 
-    println!("{} + {} = {}", args.n1, args.n2, sum);
+    // println!("{} + {} = {}", args.n1, args.n2, sum);
+    println!("{} + {} = {}", args.0, args.1, sum);
 }
