425c6e218e3e23e047291ee5466248247536cfca
Liam Reimers <liam.reimers@gmail.com>
2024-12-10 16:29:01 -0800

    Play with function declaration

--- 2024/rust/src/main.rs
+++ 2024/rust/src/main.rs
@@ -6,9 +6,20 @@ mod prelude {
 
 use crate::prelude::*;
 
+fn my_print() {
+    println!("Hello, world!");
+}
+
+fn add_numbers(n1: i32, n2: i32) -> i32 {
+    n1 + n2
+}
 
 fn main() {
-    println!("Hello, world!");
     let _ms = MyStruct {};
     let _as = Another {};
+    let n1 = 10;
+    let n2 = 20;
+    // my_print();
+    let sum = add_numbers(n1, n2);
+    println!("{} + {} = {}", n1, n2, sum);
 }
