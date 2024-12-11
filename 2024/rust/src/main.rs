ae693108e5d8fa890f977960f2c2a63dc7defabb
Liam Reimers <liam.reimers@gmail.com>
2024-12-10 20:35:22 -0800

    wip

--- 2024/rust/src/main.rs
+++ 2024/rust/src/main.rs
@@ -3,15 +3,15 @@ use std::io::{BufRead, BufReader};
 use regex::Regex;
 
 fn main() {
-    let f = File::open("./var/day_01_sample_input.txt").expect("Unable to open file");
-    // let f = File::open("./var/day_01_part_01_input.txt").expect("Unable to open file");
+    // let f = File::open("./var/day_01_sample_input.txt").expect("Unable to open file");
+    let f = File::open("./var/day_01_part_01_input.txt").expect("Unable to open file");
     let f = BufReader::new(f);
     let mut left_sum: i64 = 0;
     let mut right_sum: i64 = 0;
     for line in f.lines() {
         let mut num_index = 0;
         let line = line.expect("Unable to read line");
-        println!("{}", line);
+        // println!("{}", line);
         let re = Regex::new(r"\d+").unwrap();
         for mat in re.find_iter(line.as_str()) {
             let num_str = &line[mat.start()..mat.end()];
@@ -24,6 +24,6 @@ fn main() {
             }
             num_index += 1;
         }
-        println!(".. {}", right_sum - left_sum);
     }
+    println!("{}", right_sum - left_sum);
 }
