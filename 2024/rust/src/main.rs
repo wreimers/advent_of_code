a16e9e043a4c32318f11393463d9aada590c1e00
Liam Reimers <liam.reimers@gmail.com>
2024-12-10 16:44:13 -0800

    Play with methods

--- 2024/rust/src/main.rs
+++ 2024/rust/src/main.rs
@@ -15,8 +15,9 @@ impl SumArgs {
     fn new(n1: i32, n2: i32) -> Self {
         Self { n1, n2 }
     }
-    fn add_numbers(args: &SumArgs) -> i32 {
-        args.n1 + args.n2
+
+    fn add_numbers(&self) -> i32 {
+        self.n1 + self.n2
     }
 }
 
@@ -25,7 +26,7 @@ fn main() {
     let _as = Another {};
 
     let args = SumArgs::new(10, 20);
-    let sum = SumArgs::add_numbers(&args);
+    let sum = args.add_numbers();
 
     println!("{} + {} = {}", args.n1, args.n2, sum);
 }
