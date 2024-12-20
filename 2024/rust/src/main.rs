7c5630edad2fd32f5d4679b9dabc4947dd00dc56
Liam Reimers <liam.reimers@gmail.com>
2024-12-19 16:05:42 -0800

    d04p01 scan diagonal down-right seems to work

--- 2024/rust/src/main.rs
+++ 2024/rust/src/main.rs
@@ -84,6 +84,15 @@ fn main_day_04_part_01() {
                         println!("✅ up-left");
                     }
                 }
+                // scan diagonally down-right
+                if row_idx + 3 < rows && col_idx + 3 < cols {
+                    if file_vec[row_idx + 1][col_idx + 1] == 'M'
+                        && file_vec[row_idx + 2][col_idx + 2] == 'A'
+                        && file_vec[row_idx + 3][col_idx + 3] == 'S'
+                    {
+                        println!("✅ down-right");
+                    }
+                }
             }
         }
     }
