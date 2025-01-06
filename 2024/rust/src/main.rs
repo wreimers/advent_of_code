cc98da5d703385fe2287d318649736ef1ced417e
Liam Reimers <liam.reimers@gmail.com>
2025-01-06 07:45:21 -0800

    d11p02 put part 1 back in

--- 2024/rust/src/main.rs
+++ 2024/rust/src/main.rs
@@ -99,11 +99,69 @@ fn main_day_11_part_02() {
     dbg!(total_stones);
 }
 
-// #[derive(Debug, Clone, PartialEq, Eq, Hash)]
-// struct PlutoStone {
-//     num_str: String,
-//     // value: i64,
-// }
+#[allow(dead_code)]
+fn main_day_11_part_01() {
+    let mut stones: VecDeque<PlutoStone> = VecDeque::new();
+    let pathname = "./var/day_11_input.txt";
+    let f = File::open(pathname).expect("Unable to open file");
+    let f = BufReader::new(f);
+    for line in f.lines() {
+        let line = line.expect("Unable to read line");
+        // println!("{}", line);
+        let re = Regex::new(r"\d+").unwrap();
+        let captures: Vec<&str> = re.find_iter(line.as_str()).map(|x| x.as_str()).collect();
+        // dbg!(&captures);
+        for cap in captures {
+            stones.push_back(PlutoStone {
+                num_str: cap.to_string(),
+                value: cap.to_string().parse::<i64>().unwrap(),
+            })
+        }
+    }
+    // dbg!(&stones);
+
+    let blinks: i32 = 75;
+    for i in 0..blinks {
+        println!("blink:{}", i);
+        dbg!(&stones.len());
+        let mut newstones: VecDeque<PlutoStone> = VecDeque::new();
+        while !stones.is_empty() {
+            let stone = stones.pop_front().unwrap();
+            if stone.value == 0 {
+                newstones.push_back(PlutoStone {
+                    num_str: "1".to_string(),
+                    value: 1,
+                });
+            } else if stone.num_str.chars().count() % 2 == 0 {
+                let split_index = stone.num_str.chars().count() / 2;
+                let (first, second) = stone.num_str.split_at(split_index);
+                newstones.push_back(PlutoStone {
+                    num_str: first.to_string().parse::<i64>().unwrap().to_string(),
+                    value: first.to_string().parse::<i64>().unwrap(),
+                });
+                newstones.push_back(PlutoStone {
+                    num_str: second.to_string().parse::<i64>().unwrap().to_string(),
+                    value: second.to_string().parse::<i64>().unwrap(),
+                });
+            } else {
+                let new_value = stone.value * 2024;
+                newstones.push_back(PlutoStone {
+                    num_str: new_value.to_string(),
+                    value: new_value,
+                });
+            }
+        }
+        stones = newstones;
+    }
+    // dbg!(&stones);
+    dbg!(&stones.len());
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+struct PlutoStone {
+    num_str: String,
+    value: i64,
+}
 
 #[allow(dead_code)]
 fn main_day_10_part_02() {
